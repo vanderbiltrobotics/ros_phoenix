b58b57c61ee949d0c8859d92b75731ba072d67dc
Joseph Mathis <joseph.e.mathis@vanderbilt.edu>
2021-03-18 18:55:26 -0500

    potentiometers, etc.

---
+++ src/potentiometers.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import rospy
+from ros_phoenix.msg import MotorStatus
+from std_msgs.msg import Float32
+
+# LOW = retracted
+# HIGH = out
+
+shoulder_pub = None
+elbow_pub = None
+wrist_pub = None
+
+def interpolate(lowX, lowY, highX, highY, x):
+    changeX = highX - lowX
+    changeY = highY - lowY
+    
+    slope = float(changeX)/float(changeY)
+    
+    cons = (-1 * (slope * lowX)) + lowY
+    
+    return (slope * calculateX) + cons
+
+
+def shoulder_callback(status):
+    global shoulder_pub
+    POT_LOW = 294
+    POT_HIGH = 13
+    ANGLE_LOW = 2
+    ANGLE_HIGH = 3.35
+    
+    shoulder_pub.publish(interpolate(POT_LOW, POT_HIGH, 0, ANGLE_HIGH, status.position))
+    
+
+def elbow_callback(status):
+    global elbow_pub
+    POT_LOW = 1023
+    POT_HIGH = 747
+    ANGLE_LOW = 
+    ANGLE_HIGH = 67
+    
+    elbow_pub.publish(interpolate(POT_LOW, POT_HIGH, 0, ANGLE_HIGH, status.position))
+
+
+def wrist_callback(status):
+    global wrist_pub
+    POT_LOW = 659
+    POT_HIGH = 258
+    ANGLE_LOW = 
+    ANGLE_HIGH = 134
+    
+    wrist_pub.publish(interpolate(POT_LOW, POT_HIGH, 0, ANGLE_HIGH, status.position))
+
+
+def main():
+    global shoulder_pub, elbow_pub, wrist_pub
+    rospy.init_node("potentiometers")
+    
+    rospy.Subscriber("/shoulder/status", MotorStatus, shoulder_callback)
+    rospy.Subscriber("/elbow/status", MotorStatus, elbow_callback)
+    rospy.Subscriber("/wrist/status", MotorStatus, wrist_callback)
+    
+    shoulder_pub = rospy.Publisher("/shoulder/angle", Float32, queue_size = 10)
+    elbow_pub = rospy.Publisher("/elbow/angle", Float32, queue_size = 10)
+    wrist_pub = rospy.Publisher("/wrist/angle", Float32, queue_size = 10)
+    
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+

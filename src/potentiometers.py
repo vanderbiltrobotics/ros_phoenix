64d0eeab596f48a33e002253b1d4300ec6507e03
kushtimusPrime <kushtimusprime@gmail.com>
2021-04-16 19:02:23 -0500

    Output better arm velocities

--- src/potentiometers.py
+++ src/potentiometers.py
@@ -9,6 +9,9 @@ from std_msgs.msg import Float64
 shoulder_pub = None
 elbow_pub = None
 wrist_pub = None
+shoulder_vel_pub = None
+elbow_vel_pub = None
+wrist_vel_pub = None
 
 
 def interpolate(x1, y1, x2, y2, x):
@@ -21,42 +24,52 @@ def interpolate(x1, y1, x2, y2, x):
 
 def shoulder_callback(status):
     global shoulder_pub
+    global shoulder_vel_pub
     POT_LOW = 93
     POT_HIGH = 392
     ANGLE_LOW = 2
     ANGLE_HIGH = 3.222
 
     shoulder_pub.publish(interpolate(POT_LOW, ANGLE_LOW, POT_HIGH, ANGLE_HIGH, -status.position))
+    shoulder_vel_pub.publish(status.velocity * abs((ANGLE_HIGH - ANGLE_LOW) / (POT_HIGH - POT_LOW)))
 
 
 def elbow_callback(status):
     global elbow_pub
+    global elbow_vel_pub
     POT_LOW = 737
     POT_HIGH = 1023
     ANGLE_LOW = -0.663
     ANGLE_HIGH = 0.506
 
     elbow_pub.publish(interpolate(POT_LOW, ANGLE_LOW, POT_HIGH, ANGLE_HIGH, status.position))
+    elbow_vel_pub.publish(status.velocity * abs((ANGLE_HIGH - ANGLE_LOW) / (POT_HIGH - POT_LOW)))
 
 
 # angles from urdf
 def wrist_callback(status):
     global wrist_pub
+    global wrist_vel_pub
     POT_LOW = 265
     POT_HIGH = 668
     ANGLE_LOW = 0.174
     ANGLE_HIGH = -0.89
 
     wrist_pub.publish(interpolate(POT_LOW, ANGLE_LOW, POT_HIGH, ANGLE_HIGH, -status.position))
+    wrist_vel_pub.publish(status.velocity * abs((ANGLE_HIGH - ANGLE_LOW) / (POT_HIGH - POT_LOW)))
 
 
 def main():
-    global shoulder_pub, elbow_pub, wrist_pub
+    global shoulder_pub, elbow_pub, wrist_pub, shoulder_vel_pub, elbow_vel_pub, wrist_vel_pub
     rospy.init_node("potentiometers")
 
     shoulder_pub = rospy.Publisher("/shoulder/angle", Float64, queue_size=10)
     elbow_pub = rospy.Publisher("/elbow/angle", Float64, queue_size=10)
     wrist_pub = rospy.Publisher("/wrist/angle", Float64, queue_size=10)
+
+    shoulder_vel_pub = rospy.Publisher("/shoulder/vel", Float64, queue_size=10)
+    elbow_vel_pub = rospy.Publisher("/elbow/vel", Float64, queue_size=10)
+    wrist_vel_pub = rospy.Publisher("/wrist/vel", Float64, queue_size=10)
 
     rospy.Subscriber("/shoulder/status", MotorStatus, shoulder_callback)
     rospy.Subscriber("/elbow/status", MotorStatus, elbow_callback)

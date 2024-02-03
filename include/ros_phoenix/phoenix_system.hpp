6219ebd5b0b101aaf924ce068d06eebb2fed509a
kan-96 <101723472+kan-96@users.noreply.github.com>
2024-02-03 14:13:54 -0500

    Update phoenix_system.hpp

--- include/ros_phoenix/phoenix_system.hpp
+++ include/ros_phoenix/phoenix_system.hpp
@@ -1,3 +1,15 @@
+/*
+
+Author          Date                Description
+
+K Nguyen        2-1-2024            removed BaseInterface as base class
+
+                                    removed include header base_interface.hpp
+
+                                    added rclcpp_lifecycle/state.hpp
+
+                                    */
+
 #ifndef ROS_PHOENIX_PHOENIX_SYSTEM
 #define ROS_PHOENIX_PHOENIX_SYSTEM
 
@@ -7,12 +19,13 @@
 #include "ros_phoenix/msg/motor_control.hpp"
 #include "ros_phoenix/msg/motor_status.hpp"
 
-#include "hardware_interface/base_interface.hpp"
+// #include "hardware_interface/base_interface.hpp"
 #include "hardware_interface/system_interface.hpp"
 
 #include "rclcpp/logger.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 
 #define Phoenix_No_WPI // remove WPI dependencies
 #include "ctre/Phoenix.h"
@@ -20,7 +33,9 @@
 namespace ros_phoenix {
 
 class PhoenixSystem
-    : public hardware_interface::BaseInterface<hardware_interface::SystemInterface> {
+    // : public hardware_interface::BaseInterface<hardware_interface::SystemInterface> {
+    : public hardware_interface::<hardware_interface::SystemInterface> {
+
 public:
     static const std::string PERCENT_OUTPUT;
     static const std::string POSITION;
